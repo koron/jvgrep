b77ee64c18554d0d75689dd313925f180bf07c1f
MURAOKA Taro <koron.kaoriya@gmail.com>
2014-07-01 15:42:19 +0900

    use migemo.LoadDefault() to load a dict

--- migemo.go
+++ migemo.go
@@ -2,25 +2,11 @@ package main
 
 import (
 	gmigemo "github.com/koron/gomigemo/migemo"
-	"os"
-	"path"
 	"regexp"
 )
 
-func dictdir() string {
-	d := os.Getenv("GMIGEMO_DICTDIR")
-	if d != "" {
-		return d
-	}
-	d = os.Getenv("GOPATH")
-	if d == "" {
-		return "./_dict"
-	}
-	return path.Join(d, "src", "github.com", "koron", "gomigemo", "_dict")
-}
-
 func migemoCompile(s string) (*regexp.Regexp, error) {
-	dict, err := gmigemo.Load(dictdir())
+	dict, err := gmigemo.LoadDefault()
 	if err != nil {
 		return nil, err
 	}
